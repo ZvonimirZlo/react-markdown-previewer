f005c4f3419c7fdac68349124a1735d0ed31b190
ZvonimirZlo <lovrez2@gmail.com>
2024-05-01 23:51:34 +0200

    added bootstrap

--- react-markdown-previewer/src/App.jsx
+++ react-markdown-previewer/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import Markdown from 'react-markdown';
 
 function App() {
-  const  [input, setInput] = useState(
-  `# heading h1
+  const [input, setInput] = useState(
+    `
+  👉 Changes are re-rendered as you type.
+
+  👈 Try writing some markdown on the left.
+
+
+  # heading h1
    ## heading h2
    ### heading h3
    #### heading h4
    ##### heading h5
    ###### heading h6
-   
+
 text **bold**
 
 _italic_.
@@ -17,29 +23,32 @@ _italic_.
 **_both!_**
 
  ~~crossing~~.
- 
+
 - lists:
   - bulleted
      - two
         - three
 
-[link](https://github.com/ZvonimirZlo)
+[link](https://github.com)
 > Block Quotes
+
 
 ![React Logo w/ Text](https://assets.browserlondon.com/app/uploads/2021/02/React-king-768x432.jpg)
    `
-);
+  );
 
   return (
     <>
-    <div className="container">
-      <div>
-         <textarea autoFocus className="textarea" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <div className="row">
+        <div className='col-6'>
+          <textarea className="textarea p-4" autoFocus value={input} onChange={(e) => setInput(e.target.value)} />
+        </div>
+        <div className='col-6 p-4 output'>
+          <Markdown>{input}</Markdown>
+        </div>
       </div>
-      <div><Markdown className='output'>{input}</Markdown></div>
-    </div>
-   </>
+    </>
   )
 }
 
-export default App
+export default App;
